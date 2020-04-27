a187cde20320fc041a85a7dd5bbb98bc5d89e37f
piya <piya@piya-Lenovo-G50-80>
2020-04-27 12:53:24 +0530

    Make a new model package in that create User Entity class with annotations.

---
+++ SpringFiles/src/main/java/com/example/SpringFiles/model/User.java
@@ -0,0 +1,97 @@
+package com.example.SpringFiles.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String fname;
+	private String lname;
+	private String email;
+	private String phno;
+	private String fileType;
+	@Transient
+	private MultipartFile file;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(Long id, String fname, String lname, String email, String phno, String fileType) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+		this.email = email;
+		this.phno = phno;
+		this.fileType = fileType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhno() {
+		return phno;
+	}
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+	
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+}
